267276ebdd48749a4ce809cd4b148b849c824e2a
XFalseTaTanX <125917872+XFalseTaTanX@users.noreply.github.com>
2023-10-04 16:14:07 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,260 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Bingo humano");
+
+// Sabe Nadar 
+string respuesta;
+string nombre;
+
+Console.WriteLine("¿La persona sabe nadar?");
+respuesta = Console.ReadLine();
+
+Console.WriteLine("¿nombre de la persona?");
+nombre = Console.ReadLine();
+
+Console.WriteLine("En el campo, " + nombre + " firmo que si sabia...!!");
+
+
+//  tienen el mismo color favorito
+string respuesta2;
+string nombre2;
+
+Console.WriteLine("¿tienen el mismo color favorito?");
+respuesta2 = Console.ReadLine();
+
+Console.WriteLine("¿nombre de la persona?");
+nombre2 = Console.ReadLine();
+
+Console.WriteLine("En el campo, " + nombre2 + " firmo que si sabia...!!");
+
+
+// No le gusta el futbol
+string respuesta3;
+string nombre3;
+
+Console.WriteLine("¿No le gusta el futbol?");
+respuesta3 = Console.ReadLine();
+
+Console.WriteLine("¿nombre de la persona?");
+nombre3 = Console.ReadLine();
+
+Console.WriteLine("En el campo, " + nombre3 + " firmo que si sabia...!!");
+
+
+// le gusta el deporte
+string respuesta4;
+string nombre4;
+
+Console.WriteLine("¿le gusta el deporte?");
+respuesta4 = Console.ReadLine();
+
+Console.WriteLine("¿nombre de la persona?");
+nombre4 = Console.ReadLine();
+
+Console.WriteLine("En el campo, " + nombre4 + " firmo que si sabia...!!");
+
+
+// Nacio el mismo mes que usted
+string respuesta5;
+string nombre5;
+
+Console.WriteLine("¿Nacio el mismo mes que usted?");
+respuesta5 = Console.ReadLine();
+
+Console.WriteLine("¿nombre de la persona?");
+nombre5 = Console.ReadLine();
+
+Console.WriteLine("En el campo, " + nombre5 + " firmo que si sabia...!!");
+
+
+// Le gustan los buñuelos
+string respuesta6;
+string nombre6;
+
+Console.WriteLine("¿Le gustan los buñuelos?");
+respuesta6 = Console.ReadLine();
+
+Console.WriteLine("¿nombre de la persona?");
+nombre6 = Console.ReadLine();
+
+Console.WriteLine("En el campo, " + nombre6 + " firmo que si sabia...!!");
+
+
+// Tienen igual pasatiempo
+string respuesta7;
+string nombre7;
+
+Console.WriteLine("¿Tienen igual de pasatiempo?");
+respuesta7 = Console.ReadLine();
+
+Console.WriteLine("¿nombre de la persona?");
+nombre7 = Console.ReadLine();
+
+Console.WriteLine("En el campo, " + nombre7 + " firmo que si sabia...!!");
+
+
+// Le gusta los niños
+string respuesta8;
+string nombre8;
+
+Console.WriteLine("¿Le gusta los niños?");
+respuesta8 = Console.ReadLine();
+
+Console.WriteLine("¿nombre de la persona?");
+nombre8 = Console.ReadLine();
+
+Console.WriteLine("En el campo, " + nombre8 + " firmo que si sabia...!!");
+
+
+// Le gusta las matematicas
+string respuesta9;
+string nombre9;
+
+Console.WriteLine("¿Le gusta las matematicas?");
+respuesta9 = Console.ReadLine();
+
+Console.WriteLine("¿nombre de la persona?");
+nombre9 = Console.ReadLine();
+
+Console.WriteLine("En el campo, " + nombre9 + " firmo que si sabia...!!");
+
+
+// Tiene ojos oscuros
+string respuesta10;
+string nombre10;
+
+Console.WriteLine("¿Tiene ojos oscuros?");
+respuesta10 = Console.ReadLine();
+
+Console.WriteLine("¿nombre de la persona?");
+nombre10 = Console.ReadLine();
+
+Console.WriteLine("En el campo, " + nombre10 + " firmo que si sabia...!!");
+
+
+// Le gusta el baile
+string respuesta11;
+string nombre11;
+
+Console.WriteLine("¿Le gusta el baile?");
+respuesta11 = Console.ReadLine();
+
+Console.WriteLine("¿nombre de la persona?");
+nombre11 = Console.ReadLine();
+
+Console.WriteLine("En el campo, " + nombre11 + " firmo que si sabia...!!");
+
+
+// Es hijo unico
+string respuesta12;
+string nombre12;
+
+Console.WriteLine("¿Es hijo unico?");
+respuesta12 = Console.ReadLine();
+
+Console.WriteLine("¿nombre de la persona?");
+nombre12 = Console.ReadLine();
+
+Console.WriteLine("En el campo, " + nombre12 + " firmo que si sabia...!!");
+
+
+// Le gusta la natilla
+string respuesta13;
+string nombre13;
+
+Console.WriteLine("¿Le gusta la natilla?");
+respuesta13 = Console.ReadLine();
+
+Console.WriteLine("¿nombre de la persona?");
+nombre13 = Console.ReadLine();
+
+Console.WriteLine("En el campo, " + nombre13 + " firmo que si sabia...!!");
+
+
+// Le gusta leer
+string respuesta14;
+string nombre14;
+
+Console.WriteLine("¿Le gusta leer?");
+respuesta14 = Console.ReadLine();
+
+Console.WriteLine("¿nombre de la persona?");
+nombre14 = Console.ReadLine();
+
+Console.WriteLine("En el campo, " + nombre14 + " firmo que si sabia...!!");
+
+
+// Tiene familia en otra ciudad
+string respuesta15;
+string nombre15;
+
+Console.WriteLine("¿Tiene familia en otro lugar?");
+respuesta15 = Console.ReadLine();
+
+Console.WriteLine("¿nombre de la persona?");
+nombre15 = Console.ReadLine();
+
+Console.WriteLine("En el campo, " + nombre15 + " firmo que si sabia...!!");
+
+// Le recibi un abrazo
+string respuesta16;
+string nombre16;
+
+Console.WriteLine("¿le recibio un abrazo?");
+respuesta16 = Console.ReadLine();
+
+Console.WriteLine("¿nombre de la persona?");
+nombre16 = Console.ReadLine();
+
+Console.WriteLine("En el campo, " + nombre16 + " firmo que si sabia...!!");
+
+
+// Le gusta investigar 
+string respuesta17;
+string nombre17;
+
+Console.WriteLine("¿Le gusta investigar?");
+respuesta17 = Console.ReadLine();
+
+Console.WriteLine("¿nombre de la persona?");
+nombre17 = Console.ReadLine();
+
+Console.WriteLine("En el campo, " + nombre17 + " firmo que si sabia...!!");
+
+
+// No es de su mismo sexo
+string respuesta18;
+string nombre18;
+
+Console.WriteLine("¿No es de su mismo sexo?");
+respuesta18 = Console.ReadLine();
+
+Console.WriteLine("¿nombre de la persona?");
+nombre18 = Console.ReadLine();
+
+Console.WriteLine("En el campo, " + nombre18 + " firmo que si sabia...!!");
+
+
+// Se ha ganado algo
+string respuesta19;
+string nombre19;
+
+Console.WriteLine("¿Se ha ganado algo?");
+respuesta19 = Console.ReadLine();
+
+Console.WriteLine("¿nombre de la persona?");
+nombre19 = Console.ReadLine();
+
+Console.WriteLine("En el campo, " + nombre19 + " firmo que si sabia...!!");
+
+
+// Tiene un hijo varon menor de 18 años
+string respuesta20;
+string nombre20;
+
+Console.WriteLine("¿Se ha ganado algo?");
+respuesta20 = Console.ReadLine();
+
+Console.WriteLine("¿nombre de la persona?");
+nombre20 = Console.ReadLine();
+
+Console.WriteLine("En el campo, " + nombre20 + " firmo que si sabia...!!");
